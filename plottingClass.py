a5f7fbdd9c8467e62ec519eed2952dd17ba83aab
will-344 <willmartin344@gmail.com>
2025-11-20 13:18:15 -0500

    changed formatting in plottingClass

--- plottingClass.py
+++ plottingClass.py
@@ -5,6 +5,7 @@ class plotting:
         """
         'time' is a 1D array from initial time to end time with step size dt
         """
+        
         self.time = time 
         return
     
@@ -14,99 +15,119 @@ class plotting:
         
         'traj' is the ball trajectory in 2D array form
         """
+        
         plt.plot(traj[:,0], traj[:,1])
         plt.title("Ball Trajectory for Full Simulation")
         plt.xlabel("x Position")
         plt.ylabel("y Position")
+        plt.grid()
         plt.show()
         return
     
-    def trajectory_fluid_particle(self, traj, timespan="step", step_select=0):
+    def trajectory_fluid_particle(self, traj, timespan="full"):
         """
         This shows the trajectory of a single fluid particle over either a
         single time step or the full simulation.
         
-        timespan = "step" for a single timestep
         timespan = "full" for the full simulation
+        timespan = "step" for a single timestep
         
         'traj' is the particle trajectory in 2D array form
-        'step_select' is which step to use if plotting a single step
         """
+        
+        plt.plot(traj[:,0], traj[:,1])
         if timespan == "step":
-            plt.plot(traj[step_select,0], traj[step_select,1])
             plt.title("Single Fluid Particle Trajectory for 1 Time Step")
-            plt.xlabel("x Position")
-            plt.ylabel("y Position")
-            plt.show()
         elif timespan == "full":
-            plt.plot(traj[:,0], traj[:,1])
             plt.title("Single Fluid Particle Trajectory for Full Simulation")
-            plt.xlabel("x Position")
-            plt.ylabel("y Position")
-            plt.show()
+        plt.xlabel("x Position")
+        plt.ylabel("y Position")
+        plt.grid()
+        plt.show()
         return
     
     def velocity_vs_time(self, vel):
         """
+        This shows the velocity of the ball over time
+        
         'vel' is the velocity in 2D array form
         """
+        
         vel_mag = (vel[:,0]**2 + vel[:,1]**2)**0.5
-        plt.plot(self.time, vel_mag)
-        plt.title("Ball Velocity Magnitude vs Time")
+        plt.plot(self.time, vel_mag, label="full magnitude")
+        plt.plot(self.time, vel[:,0], label="x magnitude")
+        plt.plot(self.time, vel[:,1], label="y magnitude")
+        plt.title("Ball Velocity vs Time")
         plt.xlabel("Time")
-        plt.ylabel("Velocity Magnitude")
+        plt.ylabel("Velocity")
+        plt.legend()
+        plt.grid()
         plt.show()
         return
         
     def force_vs_time(self, force):
         """
+        This shows the force of drag on the ball over time
+        
         'force' is the drag force in 1D array form
         """
+        
         plt.plot(self.time, force)
         plt.title("Drag Force on Ball vs Time")
         plt.xlabel("Time")
         plt.ylabel("Drag Force")
+        plt.grid()
         plt.show()
         return
     
     def force_vs_velocity(self, force, vel):
         """
+        This shows the force of drag on the ball vs velocity of the ball
+        
         'force' is the drag force in 1D array form
         'vel' is the velocity in 2D array form
         """
+        
         vel_mag = (vel[:,0]**2 + vel[:,1]**2)**0.5
         plt.plot(vel_mag, force)
         plt.title("Drag Force vs Velocity Magnitude")
         plt.xlabel("Velocity Magnitude")
         plt.ylabel("Drag Force")
+        plt.grid()
         plt.show()
         return
     
     def total_energy_vs_time(self, energy):
         """
+        This shows the total system energy over time
+        
         'energy' is the total system energy in 1D array form
         """
+        
         plt.plot(self.time, energy)
         plt.title("System Energy vs Time")
         plt.xlabel("Time")
         plt.ylabel("Energy")
+        plt.grid()
         plt.show()
         return
     
     def kinetic_energy_vs_time(self, k_energy, scope="ball"):
         """
-        kinetic energy of ball over time
+        This shows kinetic energy of the ball over time
         
         scope = "ball" for ball kinetic energy
         scope = "particles" for the average kinetic energy of the particles
         
         'k_energy' is the kinetic energy in 1D array form
         """
+        
         plt.plot(self.time, k_energy)
         if scope == "ball":
             plt.title("Ball Kinetic Energy vs Time")
-        elif scope == "particles"
+        elif scope == "particles":
             plt.title("Average Kinetic Energy of Particles vs Time")
         plt.xlabel("Time")
         plt.ylabel("Energy")
+        plt.grid()
         plt.show()
