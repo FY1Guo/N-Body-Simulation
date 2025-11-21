da9a5ac33dfe5563ba48246e0ca88334686600ee
juliajvbaumgarten <juliaxbaumgarten@gmail.com>
2025-11-20 19:33:10 -0500

    Update unit_tests.py

--- unit_tests.py
+++ unit_tests.py
@@ -4,6 +4,9 @@ import gas
 import initialize
 from main import stepper, M_ball, M_gas
 
+
+""" Wall collisions"""
+
 def test_wall_collision_left_and_right():
     r0 = np.array([0.1, 0.5])
     v  = np.array([-1.0, 0.0])
@@ -17,6 +20,7 @@ def test_wall_collision_left_and_right():
    # Velocity in x should flip sign, y unchanged
    assert np.allclose(v_new, np.array([-v[0], v[1]]))
 
+""" Update projectile zero impulse """
 
 def test_update_projectile_zero_impulse_no_change():
     ball_pos = np.array([0.5, 0.5])
@@ -33,20 +37,22 @@ def test_update_projectile_zero_impulse_no_change():
     assert np.allclose(new_pos, ball_pos + ball_vel * dt)
 
 
-def test_evolve_position_no_collision_free_motion():
+""" Evolve position free motion """
+
+def test_evolve_position_free_motion():
     r0 = np.array([0.5, 0.5])
-    v = np.array([0.2, -0.1])
-
-    ball_pos = np.array([2.0, 2.0])  # far away
+    v0 = np.array([0.2, -0.1])
+    ball_pos = np.array([10.0, 10.0])   # Far away, no collisions
     ball_vel = np.array([0.0, 0.0])
-    R_ball = 0.1
-    box_size = 10.0
-    dt = 0.05
-    m_gas = 1.0
-    M_ball = 1000.0
+    R_ball   = 0.1
+    box      = 1.0
+    dt       = 0.05
 
-    r_new, v_new, dv = evolve_position(r0, v, ball_pos, ball_vel, R_ball, box_size, dt, m_gas, M_ball)
+    r_new, v_new, dv = gas.evolve_position(
+        r0, v0, ball_pos, ball_vel, R_ball,
+        box, dt, M_gas, M_ball
+    )
 
-    assert np.allclose(r_new, r0 + v * dt)
-    assert np.allclose(v_new, v)        # no collision
-    assert np.allclose(dv, np.zeros_like(v))  # no impulse on ball
+    assert np.allclose(r_new, r0 + v0 * dt)
+    assert np.allclose(v_new, v0)
+    assert np.allclose(dv, np.zeros(2))
