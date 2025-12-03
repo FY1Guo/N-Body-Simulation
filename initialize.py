007e23c20ce35648b273282f442eadbed12e840d
Carter <cmatties2003@gmail.com>
2025-12-03 13:56:23 -0500

    Speed up creation of gas position and velocity arrays

--- initialize.py
+++ initialize.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class initialize:
-    def make_particles_vel(N):
+    def make_particles_vel(N, mu_vel=464, sigma_vel=50):
         """
         Input:
         N - number of particles
@@ -10,13 +10,12 @@ class initialize:
         2xN Array of ball velocity in x-y basis
         """
 
-        initial_angles = np.asarray([np.random.uniform(0,2*np.pi) for i in range(N)]) 
-        initial_magnitudes = np.asarray([np.random.normal(loc = 464,scale = 50) for i in range(N)])
+        initial_angles = np.random.uniform(0,2*np.pi, size = N) 
+        initial_magnitudes = np.random.normal(loc = mu_vel,scale = sigma_vel, size = N)
         out_arr = np.zeros((N,2))
-        for i in range(N):
-            out_arr[i,0] += initial_magnitudes[i] * np.cos(initial_angles[i]) 
-            out_arr[i,1] += initial_magnitudes[i] * np.sin(initial_angles[i])
-
+        out_arr[:,0] = initial_magnitudes*np.cos(initial_angles)
+        out_arr[:,1] = initial_magnitudes*np.sin(initial_angles)
+        
         return out_arr
     
     def make_particles_pos(N, box_size, ball_pos, ball_radius):
@@ -28,17 +27,15 @@ class initialize:
         Array of particle positions randomly distributed within the box
         """
         
-        initial_x = np.asarray([np.random.uniform(0,1) for i in range(N)])*box_size
-        initial_y = np.asarray([np.random.uniform(0,1) for i in range(N)])*box_size
+        initial_x = np.random.uniform(0,1, size = N)*box_size
+        initial_y = np.random.uniform(0,1, size = N)*box_size
+        pos = np.array([initial_x, initial_y]).T
         for i in range(N):
-            pos_i = np.array([initial_x[i], initial_y[i]])
-            while np.sum((ball_pos-pos_i)**2)<ball_radius:
-                initial_x[i] = np.random.uniform(0,1)*box_size
-                initial_y[i] = np.random.uniform(0,1)*box_size
-                pos_i = np.array([initial_x[i], initial_y[i]])
-        out_arr = np.zeros((N,2))
-        for i in range(N):
-            out_arr[i,0] += initial_x[i]
-            out_arr[i,1] += initial_y[i]
+            while np.sum((ball_pos-pos[i])**2)<ball_radius:
+                pos[i] = np.random.uniform(0,1, size = 2)*box_size
+        #out_arr = np.zeros((N,2))
+        #for i in range(N):
+        #    out_arr[i,0] += initial_x[i]
+        #    out_arr[i,1] += initial_y[i]
 
-        return out_arr
+        return pos
